498b4604083809d7dd374aaef057035a2b0adc5e
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-06 23:47:47 +0545

    chore: align the input element

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
 
 export default function Info() {
-  const [age, setAge] = useState(18);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   return (
     <>
-    <div className="query-line flex justify-center">
-
-    <input onChange={e => setQuery(e.target.value)} value={query} type="text" placeholder="Ask a question here" />
-    <button><img className="w-8" src="src/assets/send-1-svgrepo-com.svg" alt="" /></button>
-    </div>
-    </> 
-  )
+      <div className="container m-auto max-w-80dvw">
+        <div className="query-line flex justify-center items-center bg-red-500 p-4">
+          <input
+            className="border-1 p-1"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            type="text"
+            placeholder="Ask a question here"
+          />
+          <button className="border-1">
+            <img
+              className="w-8 cursor-pointer hover:bg-red-400"
+              src="src/assets/send-1-svgrepo-com.svg"
+              alt=""
+            />
+          </button>
+        </div>
+        <p className="bg-lime-400">{query}</p>
+      </div>
+    </>
+  );
 }
