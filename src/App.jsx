cf02a03f38c212a61bd231da6615a73e5fd10597
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-07 18:36:27 +0545

    feat: show previously asked question

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { questions } from "./data";
+
+let nextId = 0;
 
 export default function Info() {
   const [query, setQuery] = useState("");
+  const [queries, setQueries] = useState([]);
 
   return (
     <>
@@ -14,15 +18,19 @@ export default function Info() {
             type="text"
             placeholder="Ask a question here"
           />
-          <button className="border-1">
+          <button id="query-btn" className="border-1" onClick={() => {setQueries([...queries, {id:nextId++, query:query}]);}  }>
             <img
-              className="w-8 cursor-pointer hover:bg-red-400"
+              className="w-8 cursor-pointer hover:bg-red-400 active:bg-red-600"
               src="src/assets/send-1-svgrepo-com.svg"
               alt=""
             />
           </button>
         </div>
-        <p className="bg-lime-400">{query}</p>
+        <ul>
+          {queries.map(que => (
+            <li key={que.id}>{que.query}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
