e25cfde7950cacaac8db79461c8e07d5d5c6d590
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-07 19:33:19 +0545

    feat: Add logo and name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { questions } from "./data";
+import './App.css';
 
 let nextId = 0;
 
@@ -10,27 +11,56 @@ export default function Info() {
   return (
     <>
       <div className="container m-auto max-w-80dvw">
-        <div className="query-line flex justify-center items-center bg-red-500 p-4">
-          <input
-            className="border-1 p-1"
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-            type="text"
-            placeholder="Ask a question here"
-          />
-          <button id="query-btn" className="border-1" onClick={() => {setQueries([...queries, {id:nextId++, query:query}]);}  }>
+        <ul>
+          {questions.map(ques => (
+            <li key={ques.id}>
+              {ques.question}
+              </li>
+          ))}
+        </ul>
+        <ul>
+          {queries.map((que) => (
+            <li key={que.id}>{que.query}</li>
+          ))}
+        </ul>
+
+        <div className="fixed bottom-0 right-0 left-0 query-line flex justify-center items-center bg-gray-100 p-4">
+          <button
+            id="query-btn"
+            className="border-1 p-2 pr-1.5 rounded-l-2xl"
+            onClick={() => {
+              setQueries([...queries, { id: nextId++, query: query }]);
+            }}
+          >
             <img
               className="w-8 cursor-pointer hover:bg-red-400 active:bg-red-600"
               src="src/assets/send-1-svgrepo-com.svg"
               alt=""
             />
           </button>
+
+          <input
+            className="border-1 p-3 pr-36 pl-2.5"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            type="text"
+            placeholder="Ask a question here"
+          />
+             <button
+            id="query-btn"
+            className="border-1 p-2 pr-1.5 rounded-r-2xl"
+            onClick={() => {
+              setQueries([...queries, { id: nextId++, query: query }]);
+            }}
+          >
+            <img
+              className="w-8 cursor-pointer hover:bg-red-400 active:bg-red-600"
+              src="src/assets/add-svgrepo-com (1).svg"
+              alt=""
+            />
+          </button>
         </div>
-        <ul>
-          {queries.map(que => (
-            <li key={que.id}>{que.query}</li>
-          ))}
-        </ul>
+        
       </div>
     </>
   );
--- src/data.js
+++ src/data.js
@@ -1,2 +1,9 @@
 export const questions = [{
-},]
+    id: 0,
+    question: 'Who is the CEO of Apple?',
+    response: 'Tim Cook',
+}, {
+    id: 1,
+    question: 'Is Apple better than Windows?',
+    response: 'Depends!',
+}];
