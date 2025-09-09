9099091ad5a57f2bee060862d82cd9402a8072fc
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-09 23:59:46 +0545

    chore: update quote generator ui

--- src/QuoteGen.jsx
+++ src/QuoteGen.jsx
@@ -17,12 +17,12 @@ export default function QuoteGenerator() {
   }
 
   return (
-    <div className="p-6 max-w-md mx-auto bg-yellow-100 rounded-xl shadow-lg text-center space-y-4">
-      <h1 className="text-xl font-bold">Random Quote Generator</h1>
+    <div className="p-6 max-w-md mx-auto bg-red-100 rounded-xl shadow-lg text-center space-y-4 my-60">
+      <h1 className="text-4xl font-bold">Random Quote Generator</h1>
       <p className="italic text-gray-700">"{quote}"</p>
       <button 
         onClick={getRandomQuote}
-        className="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600"
+        className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-purple-600"
       >
         🎲 New Quote
       </button>
