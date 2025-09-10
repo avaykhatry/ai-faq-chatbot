5d3ce78c60ed30abad6b01bb60e21037cebdb580
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-11 00:47:00 +0545

    feat: Add like,dislike, copy, speak aloud buttons to the response

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,23 @@
 function Greeting() {
-    return <h1>Curious about something?</h1>
+  return (
+    <div>
+      <h1>Curious about something?</h1>
+      <div className="logo-btm flex py-1 gap-2 items-center">
+        <div className="copy cursor-pointer">
+          <img src="src/assets/copy-svgrepo-com.svg" width="22px" alt="" />
+        </div>
+        <div className="like cursor-pointer">
+          <img src="src/assets/like-svgrepo-com.svg" width="22px" alt="" />
+        </div>
+        <div className="dislike cursor-pointer">
+          <img src="src/assets/dislike-svgrepo-com.svg" width="22px" alt="" />
+        </div>
+        <div className="speak cursor-pointer">
+            <img src="src/assets/speaker-2-svgrepo-com.svg" width="22px" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Greeting;
